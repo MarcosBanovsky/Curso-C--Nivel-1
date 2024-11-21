16bf11181f607394aa984edffcb3c846e224d44d
Marcos <marcosbanovsky@gmail.com>
2024-11-21 17:32:59 -0300

    Unidad4 - Ejercicio 5 realizado de dos formas

---
+++ unidad4/ejercicio5/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+
+5. Hacer un programa para ingresar 4 números. Luego analizar e informar por pantalla 
+si los mismos se encuentran ordenados de forma decreciente . 
+
+*/
+
+int num1, num2, num3, num4;
+
+Console.WriteLine("Ingrese un numero: ");
+num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese un numero: ");
+num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese un numero: ");
+num3 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese un numero: ");
+num4 = int.Parse(Console.ReadLine());
+
+if (num1 > num2 && num2 > num3 && num3>num4)
+{
+    Console.WriteLine("Los numeros están ordenados de forma decreciente: " + num1 + " " + num2 + " " + num3 + " "+ num4);
+ 
+}else{
+    Console.WriteLine("Los números no están ordenados de forma decreciente ");
+ 
+}
+
+/* FORMA MAL
+
+if (num1 > num2)
+{
+    if(num2 > num3)
+    {
+        if (num3 > num4)
+        {
+            Console.WriteLine("Los numeros están ordenados de forma decreciente: " + num1 + " " + num2 + " " + num3 + " "+ num4);
+        }else{
+            Console.WriteLine("Los números no están ordenados de forma decreciente ");
+        }
+    }else{
+        Console.WriteLine("Los números no están ordenados de forma decreciente ");
+
+    }
+}else{
+    Console.WriteLine("Los números no están ordenados de forma decreciente ");
+
+}
+
+*/
+
