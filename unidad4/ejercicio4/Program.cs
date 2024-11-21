33b4041a8504bbb35e6bf5717c9ddd8ea1b547e7
Marcos <marcosbanovsky@gmail.com>
2024-11-21 17:09:14 -0300

    Unidad4 - Ejercicio 4 realizado

---
+++ unidad4/ejercicio4/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+
+4. Hacer un programa para ingresar tres números y emitir un cartel aclaratorio 
+si la suma de los dos primeros es mayor al producto del segundo con el tercero.
+
+*/
+int num1, num2, num3, resultadoSuma, resultadoProducto;
+Console.WriteLine("Ingrese el primer numero");
+num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo numero");
+num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el tercer numero");
+num3 = int.Parse(Console.ReadLine());
+
+resultadoSuma = num1 + num2;
+resultadoProducto = num2 * num3;
+
+if (resultadoSuma > resultadoProducto){
+    Console.WriteLine("La suma de los dos primeros es mayor al producto del segundo con el tercero");
+}else{
+    Console.WriteLine("La suma de los dos primeros no es mayor al producto del segundo con el tercero");
+}
