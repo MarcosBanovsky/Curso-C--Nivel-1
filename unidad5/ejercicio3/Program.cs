314ebcf86e2f52f08be02f7894cbf37315149f4a
Marcos <marcosbanovsky@gmail.com>
2024-11-23 13:50:19 -0300

    Varios ejercicios realizados

---
+++ unidad5/ejercicio3/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+
+3. Hacer un programa que solicite 20 edades y luego calcule el promedio de edad 
+de aquellas personas mayores a 18 años.
+
+*/
+
+int edad = 0;
+int acuEdad = 0;
+int contMayores = 0;
+
+for(int e = 0; e < 20; e++){
+    Console.WriteLine("Ingrese una edad: ");
+    edad = int.Parse(Console.ReadLine());
+    if(edad > 18){
+        acuEdad = acuEdad + edad;
+        contMayores = contMayores + 1;
+    }
+}
+
+Console.WriteLine("El promedio de edad de los mayores de 18 es: " + acuEdad/contMayores);
