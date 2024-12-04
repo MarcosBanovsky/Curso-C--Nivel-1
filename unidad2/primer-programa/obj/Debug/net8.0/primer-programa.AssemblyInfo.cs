fbeb5ebbe56f23511b6c33962b4f78837eceddc2
Marcos <marcosbanovsky@gmail.com>
2024-12-04 17:39:18 -0300

    cambios por IDE

--- unidad2/primer-programa/obj/Debug/net8.0/primer-programa.AssemblyInfo.cs
+++ unidad2/primer-programa/obj/Debug/net8.0/primer-programa.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("primer-programa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+837980d2e2dcf1fa39db70eb95b84620c4e15860")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+547b2d0cb0666c8883c0f53fa25324ea894477e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("primer-programa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("primer-programa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
